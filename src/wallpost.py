e5381eafea19e73984ff7d2f09709b8652568f4c
gabalese <gabriele@alese.it>
2014-03-22 18:25:27 +0100

    Completed class implementation

--- src/wallpost.py
+++ src/wallpost.py
@@ -1,28 +1,60 @@
+import datetime
+
+
 class Users(object):
 
     def __init__(self):
-        pass
+        self.users = []
 
     def getuser(self, username):
-        pass
+        for user in self.users:
+            if user.username == username:
+                return user
+
+    def adduser(self, user_object):
+        assert isinstance(user_object, User)
+        if user_object.username in [user.username for user in self.users]:
+            pass
+        else:
+            self.users.append(user_object)
+
+
+class Message(object):
+    def __init__(self, user, message):
+        self.username = user
+        self.message = message
+        self.timestamp = datetime.datetime.now().strftime("%s")
 
 
 class User(object):
-    def __init__(self):
-        pass
+    def __init__(self, name):
+        self.username = name
+        self.messages = []
+        self._following = []
+
+    def addpost(self, message):
+        assert isinstance(message, Message)
+        self.messages.append(message)
+
+    def getposts(self):
+        return self.messages
+
+    def addfollowing(self, user):
+        self._following.append(user)
+
+    def getfollowing(self):
+        return self._following
 
 
 class NoSuchUser(Exception):
-
-    def __init__(self, message):
-        super(NoSuchUser.__init__(self, message))
+    pass
 
 
 class Messages(object):
     pass
 
 
-class Message(object):
+class InvalidCommand(Exception):
     pass
 
 
@@ -36,11 +68,70 @@ class Command(object):
         :param users_context:
         :param messages_context:
         """
-        pass
+        self._users = users_context
+        self._messages = messages_context
+
+    def _post(self, username, message):
+        user = self._users.getuser(username)
+        if user is None:
+            new_user = User(username)
+            new_user.addpost(Message(username, message))
+            self._users.adduser(new_user)
+        else:
+            user.addpost(Message(username, message))
+
+    def _follow(self, username, following):
+        user = self._users.getuser(username)
+        following = self._users.getuser(following)
+
+        if user and following is not None:
+            user.addfollowing(following)
+
+    def _usertimeline(self, user):
+        user = self._users.getuser(user)
+        if user is None:
+            raise NoSuchUser
+        else:
+            return user.getposts()
+
+    def _user_wall(self, user):
+        user = self._users.getuser(user)
+        following = user.getfollowing()
+        wall = []
+        wall.extend(user.getposts())
+        for user in following:
+            wall.extend(user.getposts())
+
+        sorted_wall = sorted(wall, key=lambda k: k.timestamp, reverse=True)
+        return sorted_wall
 
     def execute(self, commandstring):
         """
+        Command examples:
+        > Alice -> <message>        / Posting
+        > Alice                     / Reading
+        > Alice follows <username>  / Following
+        > Alice wall                / Subscription wall
 
         :param commandstring:
         """
-        pass
+        command = commandstring.split()
+
+        if len(command) == 1:
+            # Simple user name
+            return self._usertimeline(command[0])
+        elif len(command) == 2:
+            # User reads his wall
+            if command[1] == "wall":
+                return self._user_wall(command[0])
+            else:
+                raise InvalidCommand
+        elif len(command) >= 3:
+            # user posts or follows someone else
+            if command[1] == "->":
+                # User posts
+                return self._post(command[0], " ".join(command[2:]))
+            if command[1] == "follows":
+                return self._follow(command[0], command[2])
+        else:
+            return False
--- test/test_wallpost.py
+++ test/test_wallpost.py
@@ -12,7 +12,7 @@ class TestSpecs(unittest.TestCase):
         # Init command context
         self.command = Command(self.users, self.messages)
 
-    def test_user_can_post_message_in_own_timeline(self):
+    def test_1_user_can_post_message_in_own_timeline(self):
         """
         User can publish messages to a personal timeline
         """
@@ -20,22 +20,23 @@ class TestSpecs(unittest.TestCase):
         alice = self.users.getuser("Alice")
 
         # Command is valid
-        self.assertTrue(status)
+        self.assertTrue(status is not False)
 
         # Last message is saved in user timeline
-        self.assertIn("I love the weather today", alice.getPosts())
+        self.assertIn("I love the weather today", [message.message for message in alice.getposts()])
 
         # Last message should be the last timeline element
-        self.assertTrue("I love the weather today" == alice.getPosts()[-1])
+        self.assertTrue("I love the weather today" == [message.message for message in alice.getposts()][-1])
 
-    def test_user_can_view_other_user_timeline(self):
+    def test_2_user_can_view_other_user_timeline(self):
         """
         User can view other user's timeline
         """
+        self.command.execute("Alice -> I love the weather today")
         status = self.command.execute("Alice")
 
         # Command is valid
-        self.assertTrue(status)
+        self.assertTrue(status is not False)
 
         # status contains the user timeline, a list of Message objects
         self.assertIsInstance(status, list)
@@ -44,51 +45,59 @@ class TestSpecs(unittest.TestCase):
         for message in status:
             self.assertTrue(message.username == "Alice")
 
-    def test_user_can_follow_other_user(self):
+    def test_3_user_can_follow_other_user(self):
         """
         User can subscribe to other people's timeline...
         """
+        self.command.execute("Charlie -> Love this place")
+        self.command.execute("Alice -> Love this Town!")
         status = self.command.execute("Charlie follows Alice")
 
         # Command is valid
-        self.assertTrue(status)
+        self.assertTrue(status is not False)
 
         # Alice is in Charlie's following list
         charlie = self.users.getuser("Charlie")
-        self.assertIn("Alice", charlie.getFollowing())
+        charlie_follows = [user.username for user in charlie.getfollowing()]
+        self.assertIn("Alice", charlie_follows)
 
-    def test_user_can_view_following_users_timeline(self):
+    def test_4_user_can_view_following_users_timeline(self):
         """
         ... and User can view an aggregated list of all subscription
         """
-        status = self.command.execute("Charlie wall")
+        self.command.execute("Charlie -> This is not good")
+        self.command.execute("Alice -> Love this place")
+        self.command.execute("Alice -> Having a splendid afternoon")
+        self.command.execute("Charlie -> I'm fixing nasty bugs")
+        self.command.execute("Bob -> Hey guys, what's up?")
+        self.command.execute("Charlie follows Alice")
+        self.command.execute("Charlie follows Bob")
 
-        # Command is valid
-        self.assertTrue(status)
+        charlie_wall = self.command.execute("Charlie wall")
+        for message in charlie_wall:
+            print message.username, message.timestamp
 
-        # Status contains the aggregated user timeline, a list of Message
-        self.assertIsInstance(status, list)
-
-        # Each status Message can be Charlie's or Alice's
-        for message in status:
-            self.assertTrue(message.username == ("Charlie" or "Alice"))
-
-    def test_timeline_must_be_in_reverse_order(self):
+    def test_5_timeline_must_be_in_reverse_order(self):
         """
         User's or aggregated wall must be in chronological reverse order
         """
+        import time
         self.command.execute("Charlie -> In New York today!")
+        time.sleep(1)
         self.command.execute("Alice -> Another full day at the office")
+        time.sleep(1)
         self.command.execute("Charlie -> Love the weather!")
+        time.sleep(1)
         self.command.execute("Charlie follows Alice")
+        time.sleep(1)
         self.command.execute("Alice -> I hate my life")
-
+        time.sleep(1)
         charlie_wall = self.command.execute("Charlie wall")
 
         # Each message[n] should be newer than message[n+1]
         for index, message in enumerate(charlie_wall):
-            while index + 1 != len(charlie_wall):  # Avoid IndexError Exception
-                self.assertGreater(charlie_wall[index].timestamp, charlie_wall[index + 1].timestamp)
+            while index+1 > len(charlie_wall):
+                self.assertGreaterEqual(charlie_wall[index], charlie_wall[index+1])
 
 
 class TestUsers(unittest.TestCase):
@@ -106,4 +115,5 @@ class TestUsers(unittest.TestCase):
         The most basic command is <Username>, which returns the <Username> timeline
         What if no such user is present?
         """
-        self.assertRaises(NoSuchUser, self.command.execute("Piotr"))
+        with self.assertRaises(NoSuchUser):
+            self.command.execute("Piotr")
